db36d57c057caf8b73f2980f0d425459625b0824
BasavarajaKJ <basavarajakj06@gmail.com>
2026-02-28 12:50:31 +0530

    Feat: Add Home page CTA section

--- src/routes/(store)/_layout/index.tsx
+++ src/routes/(store)/_layout/index.tsx
@@ -1,17 +1,21 @@
+import { createFileRoute } from '@tanstack/react-router';
+
 import Hero from '@/components/templates/store/homepage/hero';
 import FeatureGrid from '@/components/templates/store/homepage/feature-grid';
 import Collection from '@/components/templates/store/homepage/collection';
-import { createFileRoute } from '@tanstack/react-router';
-
+import CTABanner from '@/components/templates/store/homepage/cta-banner';
 
 export const Route = createFileRoute('/(store)/_layout/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div className='min-h-screen'>
-    <Hero />
-    <FeatureGrid />
-    <Collection />
-  </div>
+  return (
+    <div className='min-h-screen'>
+      <Hero />
+      <FeatureGrid />
+      <Collection />
+      <CTABanner />
+    </div>
+  );
 }
