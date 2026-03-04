39689cac63c1385c55dacf8acde81e9e4cdf59d7
BasavarajaKJ <basavarajakj06@gmail.com>
2026-03-04 07:35:30 +0530

    chore: Add reused breadcrumbs nav component

--- src/routes/(store)/_layout/checkout.tsx
+++ src/routes/(store)/_layout/checkout.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import CheckoutTemplate from '@/components/templates/store/checkout/checkout-template';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/(store)/_layout/checkout')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/(store)/_layout/checkout"!</div>
+  return (
+    <div>
+      <CheckoutTemplate />
+    </div>
+  );
 }
